e79af5084401b85cb72225fdd17733ebc18c6eb6
Sargis A <sargis@kinetics-sdm.com>
2016-11-27 20:53:43 +0400

    Requirements.txt

--- voskanapat/settings.py
+++ voskanapat/settings.py
@@ -58,16 +58,29 @@ WSGI_APPLICATION = 'voskanapat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'voskanapat',
+#         'USER': 'root',
+#         'PASSWORD': 'test',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'voskanapat',
         'USER': 'root',
         'PASSWORD': 'test',
         'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'PORT': 5432,
     }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
